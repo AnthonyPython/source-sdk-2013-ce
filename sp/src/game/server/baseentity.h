d2ecc0cfb10022b6b78659ff0fcfbebf9a32734e
Nbc66 <34843947+Nbc66@users.noreply.github.com>
2022-01-25 11:40:58 +0100

    cherry-picked from master, nbc-master

--- sp/src/game/server/baseentity.h
+++ sp/src/game/server/baseentity.h
@@ -1098,7 +1098,7 @@ public:
 #ifdef _DEBUG
 	void FunctionCheck( void *pFunction, const char *name );
 
-	ENTITYFUNCPTR TouchSet( ENTITYFUNCPTR func, char *name ) 
+	ENTITYFUNCPTR TouchSet(ENTITYFUNCPTR func, const char* name)
 	{ 
 #ifdef GNUC
 		COMPILE_TIME_ASSERT( sizeof(func) == 8 );
@@ -1109,7 +1109,7 @@ public:
 		FunctionCheck( *(reinterpret_cast<void **>(&m_pfnTouch)), name ); 
 		return func;
 	}
-	USEPTR	UseSet( USEPTR func, char *name ) 
+	USEPTR	UseSet(USEPTR func, const char* name)
 	{ 
 #ifdef GNUC
 		COMPILE_TIME_ASSERT( sizeof(func) == 8 );
@@ -1120,7 +1120,7 @@ public:
 		FunctionCheck( *(reinterpret_cast<void **>(&m_pfnUse)), name ); 
 		return func;
 	}
-	ENTITYFUNCPTR	BlockedSet( ENTITYFUNCPTR func, char *name ) 
+	ENTITYFUNCPTR	BlockedSet(ENTITYFUNCPTR func, const char* name)
 	{ 
 #ifdef GNUC
 		COMPILE_TIME_ASSERT( sizeof(func) == 8 );
